850dcbb600ee793cf592c25432a23d190c52d922
Ethan <youngethan4@gmail.com>
2021-04-12 16:07:29 -0500

    Added versioning.

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -1,10 +1,11 @@
-import { Subjects } from './subjects';
-import { OrderStatus } from './types/orders-status';
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/orders-status";
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
